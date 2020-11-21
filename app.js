4e831bcb1a670e376756d4f3418389da2510674f
Bill <reborn800201@gmail.com>
2020-11-21 15:50:32 +0800

    Tracker firet Version Create

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
+const routes = require('./routes')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
+
+
+app.use(routes)
+// 設定 port 3000
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongdb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
